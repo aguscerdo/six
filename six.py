a0adf4699e5f3a5b6360f37e030ce7173279692b
aguscerdo <agus.marinovic@gmail.com>
2020-10-15 14:51:53 -0700

    try except 2

--- six.py
+++ six.py
@@ -92,15 +92,18 @@ class _LazyDescr(object):
         self.name = name
 
     def __get__(self, obj, tp):
-        result = self._resolve()
-        setattr(obj, self.name, result)  # Invokes __set__.
         try:
-            # This is a bit ugly, but it avoids running this again by
-            # removing this descriptor.
-            delattr(obj.__class__, self.name)
-        except AttributeError:
-            pass
-        return result
+            result = self._resolve()
+            setattr(obj, self.name, result)  # Invokes __set__.
+            try:
+                # This is a bit ugly, but it avoids running this again by
+                # removing this descriptor.
+                delattr(obj.__class__, self.name)
+            except AttributeError:
+                pass
+            return result
+        except ModuleNotFoundError:
+            return None
 
 
 class MovedModule(_LazyDescr):
